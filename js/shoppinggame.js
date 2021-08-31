495dd06c998655781dd661829240b9f9ae85b5a3
Johnston, Peter <peter.johnston@wizards.com>
2021-08-31 16:35:19 -0700

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -10,7 +10,7 @@ const items = 0;
 let player = {
     name, 
     score, 
-    item,
+    items,
     getCurrentScore(){
         return this.score;
     },
@@ -25,65 +25,66 @@ let player = {
 // Define the Product class - write the Constructor function for Product class here
 
 function Product(id, name, price, expiryDate){
-    this.id = id,
-    this.name = name,
-    this.price = price,
-    this.expiryDate = expiryDate,
-};
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.expiryDate = expiryDate;
+}
 
 // Complete the dateDiff function
 
 const dateDiff = (date1, date2) => {
     let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    let diffDays = Math.ciel(timeDiff / (1000 * 3600 * 24));
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
     return diffDays;
 };
 
 // Here, use Object.defineProperty to create property - daysToExpire
 
-Object.defineProperty(Product.prototype, {
-    daysToExpire:{
-        get: function() {
-            return dateDiff(this.expiryDate, new Date())
+Object.defineProperty(Product.prototype, 'daysToExpire', {
+    get: function() {
+        return dateDiff(this.expiryDate, new Date());
         }
     }
-};
+);
 
 // Add method getDetails to Product here
 
 Product.prototype.getDetails = function() {
-    return `Product Name: ${this.name} , Product Price: ${this.price}`
+    return `Product Name: ${this.name} , Product Price: ${this.price}`;
 }
 
 // Define the MagicProduct class here
 
 function MagicProduct(id, name, price, expiryDate, points, isBonus){
-    Products.call(this, id, name, price, expiryDate);
+    Product.call(this, id, name, price, expiryDate);
     this.points = points;
     this.isBonus = isBonus;
 }
 
 // Establish inheritance between Product() & MagicProduct() here
 
-MagicProducts.prototype = Object.create(Product.prototype);
+MagicProduct.prototype = Object.create(Product.prototype);
 
 // Define Rating class here
 
 class Rating {
-    conststructor(){
+    constructor(){
         this.rate = "";
     }
 
     set rating(value) {
-        if (value > 1 && value <= 4):
+        if (value > 1 && value <= 4) {
             this.rate = "OK";
-        else if (value >= 5 && value <= 7):
+        } else if (value >= 5 && value <= 7) {
             this.rate = "GOOD";
-        else if (value > 7):
+        } else if (value > 7) {
+            this.rate = "EXCEPTIONAL";
+        } else {
             this.rate = "BAD";
+        }
     }
-
-};
+}
 
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
